d25e09766b927fe89df766f6455b89147c789f22
Dušan Vystrčil <82444947+DusanVystrcil@users.noreply.github.com>
2023-03-31 20:14:49 +0200

    Move formatDateTime from Main.js to helpers folder

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,19 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { io } from "socket.io-client";
 import ChatInput from './ChatInput';
 import MessageList from "./MessageList"
-
-
-function formatDateTime(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  const milliseconds = String(date.getMilliseconds()).padStart(6, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-}
+import { formatDateTime } from '../../helpers/dateHelpers';
 
 
 const Main = ({ selectedRoom }) => {
